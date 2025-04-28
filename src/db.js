0f7f4ca54842d8b73cdb10046e7add43776e5c00
Carlos Castro <castro74carlos@gmail.com>
2025-04-28 01:27:09 -0600

    Save tweet on post request

--- src/db.js
+++ src/db.js
@@ -1,4 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
+const crypto = require('crypto');
 
 const db = new sqlite3.Database('./db/trending-hashtag-service.sqlite', (err) => {
     if (err) {
@@ -8,13 +9,66 @@ const db = new sqlite3.Database('./db/trending-hashtag-service.sqlite', (err) =>
     }
 });
 
-db.run(`
-    CREATE TABLE IF NOT EXISTS tweets (
+const runQuery = (query, params = []) => {
+    return new Promise((resolve, reject) => {
+        db.run(query, params, (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(this);
+            }
+        });
+    });
+};
+
+const createTables = async () => {
+    await createTweetsTable();
+    await createHashtagsTable();
+};
+
+const createTweetsTable = async () => {
+    const query = `CREATE TABLE IF NOT EXISTS tweets (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         hash TEXT NOT NULL UNIQUE
-    )
-`);
+    )`;
+
+    try {
+         await runQuery(query);
+         console.log('Created \'tweets\' table or it already exists');
+    } catch (err) {
+        console.error('Error creating \'tweets\' table', err);
+    }
+};
+
+const createHashtagsTable = async () => {
+    const createQuery = `CREATE TABLE IF NOT EXISTS hashtags (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        hashtag TEXT NOT NULL UNIQUE,
+        instances INTEGER NOT NULL
+    )`;
+    
+    const indexQuery = 'CREATE INDEX IF NOT EXISTS idx_instances ON hashtags (instances)';
+
+    try {
+         await runQuery(createQuery);
+         await runQuery(indexQuery);
+         console.log('Created \'hashtags\' table or it already exists');
+    } catch (err) {
+        console.error('Error creating \'hashtags\' table', err);
+    }
+};
+
+const saveTweet = async (tweet) => {
+    const hash = crypto.createHash('sha256')
+                        .update(tweet)
+                        .digest('latin1');
+    const query = 'INSERT INTO tweets (hash), VALUES (?)';
+
+    await runQuery(query, [hash]);
+};
 
 
-
-module.exports = db;
+module.exports = {
+    createTables,
+    saveTweet
+};
--- src/server.js
+++ src/server.js
@@ -7,13 +7,40 @@ const port = 3000
 
 app.use(express.json());
 
+db.createTables();
 
 app.get('/', (req, res) => {
-  res.json({"message": "Hello World!"})
+  res.status(200).json({ message: "" })
+  const { name } = req.query;
 
-  tweetQueue.add('workerHelloWorld', { name: "bob"});
-})
+  tweetQueue.add('workerHelloWorld', { name: name});
+});
+
+app.post('/tweet', (req, res) => {
+  const { tweet } = req.body;
+  
+  try {
+    if (tweet) {
+    // Attempt to persist tweet  
+    db.saveTweet(tweet);
+
+    res.status(201).json({ message: "Tweet has been registered." });
+
+    // Call worker job to process hashtags
+    tweetQueue.add('processHashtagsForTweet', { tweet: tweet });
+
+    } else {
+      res.status(400).json({ message: "Invalid value for property \'tweet\'." });
+    }
+  } catch (err) {
+    if (err.code === 'SQLITE_CONSTRAINT') {
+      res.status(400).json({ message: "Unable to register tweet, tweet has already been submitted." });
+    } else {
+      res.status(500).json({ message: "Error registering tweet." });
+    }
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Trending Hashtag Service app listening on port ${port}`)
-})
+  console.log(`Trending Hashtag Service app listening on port: ${port}`);
+});
